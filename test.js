f26873ccbc2cd4db59508f1c0822f5e11ea2eee1
kenokabe <kenokabe@gmail.com>
2015-01-22 08:12:53 +0900

    commit

--- test.js
+++ test.js
@@ -5,16 +5,36 @@ var $ = spinoza.$;
 var compute = spinoza.compute;
 var out = spinoza.out;
 
-var plus10 = function(fs)
-{
+var plus10 = function(fs) {
   var _ = require('lodash');
-  var f = function(x)
-  {
+  var f = function(x) {
     return x + 10;
   };
   var z = _.map(fs.s, f);
   return z;
 };
+
+var f = function() {
+  world = $('delay hello')(out);
+};
+var delay = setTimeout(f, 2000);
+
+//-------------
+
+var input = (function() {
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+　
+  process.stdin.on('data', function(chunk) {
+    chunk.trim().split('\n').forEach(function(line) {
+  　
+      world = $(line)(out);
+    });
+  });
+　
+  process.stdin.on('end', function() {});
+})();
+//-----------
 
 //===========================================
 
@@ -36,5 +56,9 @@ world = $(1)(2)(3)(plus10)(out);
 
 world = $('hello');
 //　nothing happens
+
+world = $('hello')(out)(out);
+// hello twice
+
 
 //===========================================
